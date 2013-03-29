c1005c9a4f1acd638d39c78d4f5aa4352a7703cb
John Dyer <johndyer@gmail.com>
2013-03-29 07:02:48 -0500

    cleanup, enable single file

--- indexer.py
+++ indexer.py
@@ -6,6 +6,11 @@
 __author__      = "John Dyer"
 __copyright__   = "Copyright 2013"
 
+"""
+Dependencies: Python 2.7, pyquery, stemming
+Usage: indexer.py -i '../browserbible/app/content/bibles/en_nasb/'
+"""
+
 import os, sys, getopt
 from pyquery import PyQuery
 from stemming.porter2 import stem
@@ -14,14 +19,17 @@ def main(argv):
 
 	path = ''
 	enable_stem = False
+	single_chapter = ''
 
 	# get options
-	opts, args = getopt.getopt(argv, 'i:s:')
+	opts, args = getopt.getopt(argv, 'i:s:c:')
 	for opt, arg in opts:                
 		if opt in ("-i"):      
 			path = arg
 		elif opt in ("-s"):      
-			enable_stem = True		
+			enable_stem = True	
+		elif opt in ("-c"):      
+			single_chapter = arg					
 			
 	if path == '':
 		print 'no path defined'
@@ -30,54 +38,49 @@ def main(argv):
 	# create index path	
 	index_path = path + '/index/'
 	if not os.path.exists(index_path): 
-		os.makedirs(index_path)
-	else:
+		os.makedirs(index_path)		
+	
+	# do indexing
+	if (single_chapter != '')
+		process_chapter(path + single_chapter, index_path, enable_stem)
+	elif
 		#delete all exising files
 		for the_file in os.listdir(index_path):
 			file_path = os.path.join(index_path, the_file)
-			os.unlink(file_path)			
-	
-	#demo chapter
-	#process_chapter(path + 'Gen.1.html', index_path)
-	#
-			
-	#exit()
-	
-	# go through all files
-	'''
-	for the_file in os.listdir(path):
-		if the_file.endswith(".html") and not the_file.endswith("index.html"):
-			chapter_path = 	os.path.join(path, the_file)
-			process_chapter(chapter_path, index_path);
-	'''
-	# go through osis book names
-	osis_names = ["Gen", "Exod", "Lev", "Num", "Deut", "Josh", "Judg", "Ruth", "1Sam", "2Sam", "1Kgs", "2Kgs", "1Chr", "2Chr", "Ezra", "Neh", "Esth", "Job", "Ps", "Prov", "Eccl", "Song", "Isa", "Jer", "Lam", "Ezek", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nah", "Hab", "Zeph", "Hag", "Zech", "Mal", "Matt", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phil", "Col", "1Thess", "2Thess", "1Tim", "2Tim", "Titus", "Phlm", "Heb", "Jas", "1Pet", "2Pet", "1John", "2John", "3John", "Jude", "Rev"]
-	
-	for osis_book in osis_names:
-		for chapter_num in range(1,150):
-			chapter_osis = osis_book + '.' + str(chapter_num)
-			chapter_path = os.path.join(path, chapter_osis + '.html')
-			
-			if os.path.exists(chapter_path):
-				process_chapter(chapter_path, index_path, enable_stem)
-			else:
-				break
-	
-	
-	
+			os.unlink(file_path)		
+
+		# go through osis book names to keep things in order
+		osis_names = ["Gen", "Exod", "Lev", "Num", "Deut", "Josh", "Judg", "Ruth", "1Sam", "2Sam", "1Kgs", "2Kgs", "1Chr", "2Chr", "Ezra", "Neh", "Esth", "Job", "Ps", "Prov", "Eccl", "Song", "Isa", "Jer", "Lam", "Ezek", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nah", "Hab", "Zeph", "Hag", "Zech", "Mal", "Matt", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phil", "Col", "1Thess", "2Thess", "1Tim", "2Tim", "Titus", "Phlm", "Heb", "Jas", "1Pet", "2Pet", "1John", "2John", "3John", "Jude", "Rev"]
+		
+		for osis_book in osis_names:
+			for chapter_num in range(1,150):
+				chapter_osis = osis_book + '.' + str(chapter_num)
+				chapter_path = os.path.join(path, chapter_osis + '.html')
+				
+				if os.path.exists(chapter_path):
+					process_chapter(chapter_path, index_path, enable_stem)
+				else:
+					break
+		
 	# close all files
 	cleanup_index(index_path)
 			
 def process_chapter(chapter_path, index_path, enable_stem) :
+
+	# removed characters
+	remove_chars = ['.',',',';','?''!','-',u'–',u'―',u'—',u'~',':','"',')','(','[',']','/',"'s",u'’s',"'",u'‘',u'’',u'“',u'”']
+
+	# create jQuery object
 	html = open(chapter_path, 'r').read()
 	jquery = PyQuery(html)
 	
 	print jquery.find('.chapter').attr('data-osis')
 	
+	# find all verses, remove all notes and verse numbers
 	verses = jquery('span.verse')
 	verses.find('span.note').remove()
 	verses.find('span.cf').remove()
-	verses.find('.v-num').remove()			
+	verses.find('.v-num').remove()
 	
 	for verse in verses:
 		v = PyQuery(verse)
